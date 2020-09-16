b2d6fb650190e30980d1b5635267198a29513a24
zksJack <44696770+zksJack@users.noreply.github.com>
2020-09-16 17:09:59 +0800

    修改端口号 5000

--- dao/mysqlDB.js
+++ dao/mysqlDB.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
+  host     : '118.178.85.48',
   user     : 'root',
   password : '123456',
-  database : 'djsystem'
+  database : 'DJServer'
 });
  
 module.exports  = connection;
