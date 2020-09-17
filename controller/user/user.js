f3d981b81865fc13b7769b78fbdb4f0cad857ef9
zksJack <44696770+zksJack@users.noreply.github.com>
2020-09-17 17:12:32 +0800

    用修改 查询条件

--- controller/user/user.js
+++ controller/user/user.js
@@ -8,14 +8,14 @@ const upload = multer({ storage: multer.memoryStorage() }) // 上传文件使用
 // 引入controller的逻辑处理函数
 let userService = require("../../service/user/user");
 //导入校验规则对象
-let {user_schema} = require("../../shema/user/user");
+let {user_schema,user_name_schema} = require("../../shema/user/user");
 let Router = express.Router();
 
 Router.post("/getAllUserInfo",userService.getAllUserInfo);
 Router.post("/getUserInfo",expressJoi(user_schema),userService.getUserInfo);
 Router.post("/resetPWD",expressJoi(user_schema),userService.resetPWD);
 Router.post("/operationState",expressJoi(user_schema),userService.operationState);
-Router.post("/getUserByName",expressJoi(user_schema),userService.getUserByName);
+Router.post("/getUserByName",expressJoi(user_name_schema),userService.getUserByName);
 Router.post("/batchReset",userService.batchReset);
 Router.post("/uploadExcel",upload.any(),userService.uploadExcel);
 
--- shema/user/user.js
+++ shema/user/user.js
@@ -1,9 +1,15 @@
 let Joi = require("@hapi/joi");
 let userID = Joi.string().max(30).required();
+let username = Joi.string().max(30).required();
 let disabled = Joi.string()
 exports.user_schema={
     body:{
         userID,
         disabled
+    }
+}
+exports.user_name_schema={
+    body:{
+        username
     }
 }
