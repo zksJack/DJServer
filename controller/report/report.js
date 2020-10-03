269d4e85b8bdd6c401948890fa58a31e5136c464
zksJack <44696770+zksJack@users.noreply.github.com>
2020-10-03 15:56:49 +0800

    增加思想报告校验规则

--- controller/report/report.js
+++ controller/report/report.js
@@ -1,7 +1,7 @@
 let express = require("express");
 let reportService = require("../../service/report/report");
 let expressJoi = require('@escook/express-joi');
-let {up_Accept_schema,in_report_schema} = require('../../shema/report/report')
+let {in_report_schema,updateAccept_schema,batchAccept_schema} = require('../../shema/report/report')
 //配置multer解析表单数据
 let multer = require('multer');
 let path = require('path');
@@ -9,8 +9,8 @@ let upload = multer({dest:path.join(__dirname,"../../uploads")})
 let Router = express.Router();
 Router.post("/pageQuery",reportService.pageQuery);
 Router.post("/selectById",reportService.selectById);
-Router.post("/updateAccept",expressJoi(up_Accept_schema),reportService.updateAccept);
-Router.post("/batchAccept",reportService.batchAccept);
+Router.post("/updateAccept",expressJoi(updateAccept_schema),reportService.updateAccept);
+Router.post("/batchAccept",expressJoi(batchAccept_schema),reportService.batchAccept);
 Router.post("/insertReport",upload.array('files',5),expressJoi(in_report_schema),reportService.insertReport);
 Router.post("/selectAccept",expressJoi(in_report_schema),reportService.selectAccept);
 module.exports =Router
--- shema/report/report.js
+++ shema/report/report.js
@@ -7,13 +7,6 @@ exports.in_schema={
         reportId:JoiUtils.IS_STRING_NOT_NULL
    }
 }
-exports.up_Accept_schema={
-    body:{
-        is_accept:JoiUtils.IS_INT_NOT_NULL,
-        reason:JoiUtils.IS_STRING_NOT_NULL,
-        reportId:JoiUtils.IS_STRING_NOT_NULL
-    }
- }
  exports.in_report_schema={
     body:{
         istype:JoiUtils.IS_INT_NOT_NULL,
