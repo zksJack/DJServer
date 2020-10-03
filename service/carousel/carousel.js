a778aea851ab5cd7ed19376c6ff74700a978fa0d
zksJack <44696770+zksJack@users.noreply.github.com>
2020-10-03 11:30:51 +0800

    插入轮播图调整

--- service/carousel/carousel.js
+++ service/carousel/carousel.js
@@ -85,7 +85,7 @@ exports.batchDelete = (req, res) => {
 //
 //插入轮播信息
 exports.insertById = (req, res) => {
-    let sql = 'INSERT INTO tb_carousel(title,url, img_url, priority ,type,status ,create_time) VALUES(?,?,?,?,?,?,?,now())';
+    let sql = 'INSERT INTO tb_carousel(title,url, img_url, priority ,type,status ,create_time) VALUES(?,?,?,?,?,?,now())';
     let sqlParams = [
         req.body.title,
         req.body.url,
